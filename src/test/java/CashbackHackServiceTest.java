e8d2b043900d9476207651dc5aef38815f7c493b
Anastasia I <niavolk@gmail.com>
2023-03-26 16:47:47 +0400

    Changed dependencies to testng and added tests

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,46 @@
+import org.testng.annotations.Test;
+import ru.netology.CashbackHackService;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void Check1000Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Check999Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Check1001Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1001);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Check2000Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2500);
+        int expected = 500;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void Check600Amount() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(600);
+        int expected = 400;
+        assertEquals(actual, expected);
+    }
+}
